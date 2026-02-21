454b01d1fe368284b9319abee90517fb2033e78a
Tóth Attila <atoth.german@gmail.com>
2026-02-21 15:15:07 +0100

    My first try

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿module InputValueModule
+open System
+
+type Intervall = {
+    Also: int
+    Felso: int
+}
+
+let rec private readInt prompt =
+    printf "%s" prompt
+    let input= Console.ReadLine()
+    match Int32.TryParse(input) with
+    | true, value -> value 
+    | false, _ ->
+        printfn "Hibás szám, újra!"
+        readInt prompt
+let bekeresIntervall() =
+    let also = readInt "Alsó határ:"
+    let felso = readInt "Felso hatar:"
+    { Also = also; Felso = felso }
+
+[<EntryPoint>]
+let main argv =
+    let interval = bekeresIntervall()
+    printfn "Az intervallum [%d; %d] " interval.Also interval.Felso
+    0
